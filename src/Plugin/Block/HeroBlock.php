e8e9a55a772f54d4275c9d1cf67d59e3c1dad3bc
quipcode <abdulkarim8ahmed@gmail.com>
2021-06-27 18:54:35 -0700

    display table in block via renderable table array https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Render%21Element%21Table.php/class/Table/9.1.x

--- src/Plugin/Block/HeroBlock.php
+++ src/Plugin/Block/HeroBlock.php
@@ -19,10 +19,41 @@ class HeroBlock extends BlockBase{
     * {@inheritdoc}
      */
     public function build(){
-        return [
-            '#type' => 'markup',
-            '#markup' => $this->t('the output of our superheroes block')
-        ]; 
+        $heroes = [
+            ['hero_name' => 'Thor', 'real_name' => 'Thor Odinson'],
+            ['hero_name' => 'Captain America', 'real_name' => 'Steve Rogers'],
+            ['hero_name' => 'Wolverine', 'real_name' => 'James Howlett'],
+            ['hero_name' => 'Phoenix', 'real_name' => 'Jean Grey'],
+            ['hero_name' => 'Wonder Woman', 'real_name' => 'Diane Prince'],
+            ['hero_name' => 'Batman', 'real_name' => 'Bruce Wayne'],
+            ['hero_name' => 'Superman', 'real_name' => 'Clark Kent'],
+            ['hero_name' => 'Spider-Man', 'real_name' => 'Peter Parker']
+        ];
+
+        $table = [
+            '#type' => 'table',
+            '#header' => [
+                $this->t('Hero Name'),
+                $this->t('Real Name'),
+            ]
+
+            ]; 
+
+            foreach($heroes as $hero){
+                $table[] = [
+                        'hero_name' =>[
+                            '#type' => 'markup',
+                            '#markup' => $hero['hero_name']
+                        ],
+                        'real_name' => [
+                            '#type' => 'markup',
+                            '#markup' => $hero['real_name']
+                        ]
+                    ];
+            }
+            return $table;
+
+     
     }
 
 }
