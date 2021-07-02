f1a71acf99fe4dc29937a9847d82e115df28430d
quipcode <abdulkarim8ahmed@gmail.com>
2021-07-01 17:09:43 -0700

    got custom hero article service up and running, pulls all articles on '/drupal/heroarticles', in addition the articles are listed in an unordered list providing link to article itself, shows and links user who wrote the article, and displays created and last modified time in SQL format and shown in current user's timeszone

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -4,7 +4,7 @@ namespace Drupal\module_hero\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\module_hero\Services\HeroArticleService;
-
+use Drupal\user\Entity;
 
 
 
@@ -19,29 +19,63 @@ include 'kint.phar';
 * */
 class HeroController extends ControllerBase{
 
-    
-    
+    /**
+     * The hero article service
+     * @var \Drupal\module_hero\Services\HeroArticleService
+     */
+    protected $articleHeroService;
 
-    private $articleHeroService;
+    /**
+     * HeroController constructor.
+     *
+     * @param \Drupal\module_hero\Services\HeroArticleService $articleHeroService
+     *   The service
+     */
+    public function __construct(HeroArticleService $articleHeroService){
+        $this->articleHeroService = $articleHeroService;
+    }
 
+    
+    /**
+     * {@inheritdoc}
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     *   The Drupal service container.
+     *
+     * @return static
+     */
     public static function create(ContainerInterface $container){
         return new static(
             $container->get('module_hero.hero_articles')
         );
     }
 
-    public function __construct(HeroArticleService $articleHeroService){
-        $this->articleHeroService = $articleHeroService;
+    public function getCurrUser(){
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        return $user;
     }
 
+  public function heroArticles(){
+    //   d($this->articleHeroService->getHeroArticles());
+        $articles = $this->articleHeroService->getHeroArticles();
+        $currUser = $this->getCurrUser();
+        return [
+            '#theme' => 'hero_articles',
+           '#items' => $articles,
+           '#current_user' => $currUser,
+           '#title' => $this->t('Articles about our heroes'),
+        ];
+        
+    
+  }
+
     public function heroList(){
+        // d($this->articleHeroService->getHeroArticles());
+
+
+
         // Kint::dump('dumped with kint');
         // d('Dumped with Kint');
-        // private $service = \Drupal::service('module_hero.hero_articles');
-        d($this->articleHeroService->getHeroArticles());
-        // d($this->articleHeroService->getHeroArticles);
-        // Kint::dump($this->articleHeroService); die();
-        // kint($this->articleHeroService); die();
         $heroes = [
             ['name' => 'Thor'],
             ['name' => 'Captain America'],
--- src/Services/HeroArticleService.php
+++ src/Services/HeroArticleService.php
@@ -2,9 +2,20 @@
 
 namespace Drupal\module_hero\Services;
 
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+
 class HeroArticleService{
   public function getHeroArticles(){
+
+        $nids = \Drupal::entityQuery('node')->condition('type','article')->execute();
+        $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+
+        $nid = 1;
+        $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+        
     $articles = ["Hulk is mad", "Flash is fast"];
-    return $articles;
+    
+    return $nodes;
   }
 }
